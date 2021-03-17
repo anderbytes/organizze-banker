4da3a81670a504cac9b39709f6a45096243a6684
anderbytes <anderbytes@users.noreply.github.com>
2021-03-17 14:42:10 -0300

    Melhorias diversos no código e limpeza

--- auxiliar.py
+++ auxiliar.py
@@ -1,6 +1,16 @@
-def validaData(texto):
+def validaData(self, texto: str):
     try:
         from datetime import datetime
         datetime.strptime(texto, "%Y-%m-%d")
     except ValueError:
         raise ValueError("O formato da data é inválida, esperado: AAAA-MM-DD")
+
+
+def validaAno(self, numero: int, minimo: int, maximo: int):
+    if not (minimo <= numero <= maximo):
+        raise ValueError("O ano informado não é considerado válido.")
+
+
+def enumera(lista: list):
+    for i, t in enumerate(lista):
+        print(f'[{i}] => {t}')
--- banker.py
+++ banker.py
@@ -1,99 +1,113 @@
 class Banker:
     import requests
     from requests.auth import HTTPBasicAuth
-    from auxiliar import validaData
+    from auxiliar import validaData, validaAno
 
     # Parametros oriundos da documentação da API da Organizze
-    URL_API = 'https://api.organizze.com.br/rest/v2/'
-    CONTENT_TYPE = {'Content-Type': 'application/json; charset=utf-8'}
+    URL_API = "https://api.organizze.com.br/rest/v2"
 
-    def __init__(self, email, token, autor='SemNome'):
-        if email is None:
-            raise ValueError('Preencha um Email no 1º parâmetro')
-        if token is None:
-            raise ValueError('Preencha um Token no 2º parâmetro')
-        if not isinstance(autor, str):
-            raise ValueError('O campo AUTOR precisa conter uma string')
-
+    def __init__(self, email: str, token: str, autor: str = 'SemNome'):
         self.sessao = self.requests.Session()
         self.sessao.auth = self.HTTPBasicAuth(email, token)
-        self.sessao.headers.update(self.CONTENT_TYPE)
-        self.sessao.headers.update({'User-Agent': f'{autor} ({email})'})
+        self.sessao.headers.update({'User-Agent': f'{autor} ({email})', 'Content-Type': 'application/json; charset=utf-8'})
 
     ## USUARIOS #################################################################
-    def getUsuarios(self):
-        return self.sessao.get(self.URL_API + "users/").json()
+    def getUsuarios(self) -> list:
+        return self.sessao.get(f'{self.URL_API}/users').json()
 
-    def getUsuario(self, idUsuario):
-        return self.sessao.get(self.URL_API + "users/" + str(idUsuario)).json()
+    def getUsuario(self, idUsuario: int) -> dict:
+        return self.sessao.get(f'{self.URL_API}/users/{idUsuario}').json()
 
     ## CONTAS   #################################################################
-    def getContas(self):
-        return self.sessao.get(self.URL_API + "accounts/").json()
+    def getContas(self) -> list:
+        return self.sessao.get(f'{self.URL_API}/accounts').json()
 
-    def getConta(self, idConta):
-        return self.sessao.get(self.URL_API + "accounts/" + str(idConta)).json()
+    def getConta(self, idConta: int) -> dict:
+        return self.sessao.get(f'{self.URL_API}/accounts/{idConta}').json()
+
+    def addConta(self, JSON_params):
+        self.sessao.post(f'{self.URL_API}/accounts', params=JSON_params)
+
+    def updConta(self, idConta: int, JSON_params):
+        self.sessao.put(f'{self.URL_API}/accounts/{idConta}', params=JSON_params)
+
+    def delConta(self, idConta: int):
+        self.sessao.delete(f'{self.URL_API}/accounts/{idConta}')
 
     ## METAS    #################################################################
-    def getMetas(self):
-        return self.sessao.get(self.URL_API + "budgets/").json()
+    def getMetas(self, ano: int = 0, mes: int = 0) -> list:
+        parametros = ""
+        if (ano > 0):
+            self.validaAno(ano, 1900, 3000)
+            parametros = f'{ano}'
+            if (1 <= mes <= 12):
+                parametros = f'{parametros}/{mes}'
+        return self.sessao.get(f'{self.URL_API}/budgets/{parametros}').json()
 
     ## CATEGORIAS ###############################################################
-    def getCategorias(self):
-        return self.sessao.get(self.URL_API + "categories/").json()
+    def getCategorias(self) -> list:
+        return self.sessao.get(f'{self.URL_API}/categories').json()
 
-    def getCategoria(self, idCategoria):
-        return self.sessao.get(self.URL_API + "categories/" + str(idCategoria)).json()
+    def getCategoria(self, idCategoria: int) -> dict:
+        return self.sessao.get(f'{self.URL_API}/categories/{idCategoria}').json()
 
-    def updCategoria(self, idCategoria, JSON_params):
-        self.sessao.put(self.URL_API + "categories/" + str(idCategoria), params=JSON_params)
+    def addCategoria(self, JSON_params):
+        self.sessao.post(f'{self.URL_API}/categories', params=JSON_params)
 
-    def delCategoria(self, idCategoria, idNovaCategoria):
-        self.sessao.delete(self.URL_API + "categories/" + str(idCategoria), params={'replacement_id': idNovaCategoria})
+    def updCategoria(self, idCategoria: int, JSON_params):
+        self.sessao.put(f'{self.URL_API}/categories/{idCategoria}', params=JSON_params)
+
+    def delCategoria(self, idCategoria: int, idNovaCategoria: int = None):
+        if (idNovaCategoria is not None):
+            self.sessao.delete(f'{self.URL_API}/categories/{idCategoria}', params={'replacement_id': idNovaCategoria})
+        else:
+            self.sessao.delete(f'{self.URL_API}/categories/{idCategoria}')
 
     ## CARTÕES DE CRÉDITO #######################################################
-    def getCartoesCredito(self):
-        return self.sessao.get(self.URL_API + "credit_cards/").json()
+    def getCartoesCredito(self) -> list:
+        return self.sessao.get(f'{self.URL_API}/credit_cards').json()
 
-    def getCartaoCredito(self, idCartao):
-        return self.sessao.get(self.URL_API + "credit_cards/" + idCartao).json()
+    def getCartaoCredito(self, idCartao: int) -> dict:
+        return self.sessao.get(f'{self.URL_API}/credit_cards/{idCartao}').json()
 
-    def updCartaoCredito(self, idCartao, JSON_params):
-        self.sessao.put(self.URL_API + "credit_cards/" + str(idCartao), params=JSON_params)
+    def addCartaoCredito(self, JSON_params):
+        self.sessao.post(f'{self.URL_API}/credit_cards', params=JSON_params)
 
-    def delCartaoCredito(self, idCartao):
-        self.sessao.delete(self.URL_API + "credit_cards/" + str(idCartao))
+    def updCartaoCredito(self, idCartao: int, JSON_params):
+        self.sessao.put(f'{self.URL_API}/credit_cards/{idCartao}', params=JSON_params)
 
-    def getFaturasCartao(self, idCartao):
-        return self.sessao.get(self.URL_API + "credit_cards/" + idCartao + "/invoices").json()
+    def delCartaoCredito(self, idCartao: int):
+        self.sessao.delete(f'{self.URL_API}/credit_cards/{idCartao}')
 
-    def getFaturaCartao(self, idCartao, idFatura):
-        return self.sessao.get(self.URL_API + "credit_cards/" + idCartao + "/invoices" + str(idFatura)).json()
+    def getFaturasCartao(self, idCartao: int) -> list:
+        return self.sessao.get(f'{self.URL_API}/credit_cards/{idCartao}/invoices').json()
 
-    def getPagamentosFatura(self, idCartao, idFatura):
-        return self.sessao.get(
-            self.URL_API + "credit_cards/" + idCartao + "/invoices" + str(idFatura) + "/payments").json()
+    def getFaturaCartao(self, idCartao: int, idFatura: int) -> dict:
+        return self.sessao.get(f'{self.URL_API}/credit_cards/{idCartao}/invoices/{idFatura}').json()
+
+    def getPagamentosFatura(self, idCartao: int, idFatura: int) -> list:
+        return self.sessao.get(f'{self.URL_API}/credit_cards/{idCartao}/invoices/{idFatura}/payments').json()
 
     ## LANÇAMENTOS #################################################################
-    def getLancamentos(self, dataInicio=None, dataFim=None):
+    def getLancamentos(self, dataInicio: str = "AAAA-MM-DD", dataFim: str = "AAAA-MM-DD") -> list:
+        parametros = ""
+        if (dataInicio != "AAAA-MM-DD"):
+            self.validaData(dataInicio)
+            parametros = f'&start_date={dataInicio}'
+        if (dataFim != "AAAA-MM-DD"):
+            self.validaData(dataFim)
+            parametros = f'{parametros}&end_date={dataFim}'
+        return self.sessao.get(f'{self.URL_API}/transactions', params=parametros).json()
 
-        if (dataInicio is None and dataFim is None):
-            return self.sessao.get(self.URL_API + "transactions/").json()
-        else:
-            parametros = ""
-            if (dataInicio is not None):
-                self.validaData(dataInicio)
-                parametros = "&start_date=" + dataInicio
-            if (dataFim is not None):
-                self.validaData(dataFim)
-                parametros += ("&end_date=" + dataFim)
-            return self.sessao.get(self.URL_API + "transactions/", params=parametros).json()
+    def getLancamento(self, idLancamento: int) -> dict:
+        return self.sessao.get(f'{self.URL_API}/transactions/{idLancamento}').json()
 
-    def getLancamento(self, idTransacao):
-        return self.sessao.get(self.URL_API + "transactions/" + str(idTransacao)).json()
+#def addLancamento(self, JSON_params):
+#def addLancamentoFixo(self, JSON_params):
+#def addLancamentoParcelado(self, JSON_params):
 
-    def updLancamento(self, idLancamento, JSON_params):
-        self.sessao.put(self.URL_API + "transactions/" + str(idLancamento), params=JSON_params)
+    def updLancamento(self, idLancamento: int, JSON_params):
+        self.sessao.put(f'{self.URL_API}/transactions/{idLancamento}', params=JSON_params)
 
-    def delLancamento(self, idLancamento):
-        self.sessao.delete(self.URL_API + "transactions/" + str(idLancamento))
+    def delLancamento(self, idLancamento: int):
+        self.sessao.delete(f'{self.URL_API}/transactions/{idLancamento}')
